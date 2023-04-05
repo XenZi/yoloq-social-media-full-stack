f98d146fcd9a863b6eae54cfb089fdc76679b823
XenZi <rilakmateja@gmail.com>
2023-04-05 20:06:54 +0200

    setting up initial folder structure of the project

---
+++ yoloq/src/main/java/com/example/yoloq/YoloqApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoloq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoloqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoloqApplication.class, args);
+	}
+
+}
